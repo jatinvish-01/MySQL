37bf1e11fd2d65307c538e2fc68b2c266c1e2b88
Jatin Vishwakarma <vishwakarmajatin084@gmail.com>
2025-07-20 11:40:23 +0530

    initial commit

--- join.sql
+++ join.sql
@@ -1,8 +1,33 @@
-SELECT students.name, marks.subject, marks.score from students inner join marks on students.id = marks.student_id;
+CREATE TABLE Customers(
+	CustomersID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(100),
+    Email VARCHAR(100) UNIQUE,
+    City VARCHAR(50)
+);
 
-SELECT students.name, marks.subject, marks.score from students left join marks on students.id = marks.student_id;
+INSERT INTO Customers(Name, Email, City) 
+VALUES 
+('Amit Kumar', 'amitkumar@gmail.com', 'Delhi, India'),
+('Neha Verma','nehaverma@gmail.com', 'Bangalore, India'),
+('Rahul Sharma', 'rahulsharma@gmail.com', 'Kolkata, India');
 
-SELECT students.name, marks.subject, marks.score from students right join marks on students.id = marks.student_id;
+SELECT * FROM Customers;
 
-SELECT students.name, marks.subject, marks.score from students cross join marks;
+-- Updating the table
+
+UPDATE Customers
+SET City = 'Pune'
+WHERE NAME = 'Rahul Sharma';
+
+SELECT * FROM Customers;
+
+-- Deleting the table data
+
+DELETE FROM Customers
+WHERE Name = 'Neha Verma'; 
+SELECT * FROM Customers;
+
+
+
+
 
